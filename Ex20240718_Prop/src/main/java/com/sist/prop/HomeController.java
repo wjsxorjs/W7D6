247f60e7520cc4184b3ca3f06e7326f70a707509
wjsxorjs <wjsxorjs@naver.com>
2024-07-18 13:58:15 +0900

    20240718 Spring 0.5

--- Ex20240718_Prop/src/main/java/com/sist/prop/HomeController.java
+++ Ex20240718_Prop/src/main/java/com/sist/prop/HomeController.java
@@ -1,15 +1,16 @@
 package com.sist.prop;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import mybatis.dao.DeptDAO;
+import mybatis.vo.DeptVO;
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +18,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	@Autowired
+	private DeptDAO d_dao;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		DeptVO[] d_ar = d_dao.total();
 		
-		String formattedDate = dateFormat.format(date);
+		mv.addObject("d_ar", d_ar);
+		mv.setViewName("dept/list");
 		
-		model.addAttribute("serverTime", formattedDate );
+		return mv;
 		
-		return "home";
+			
+		
 	}
 	
 }
